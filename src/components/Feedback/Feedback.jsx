b386001a7d3b96effa496524090d9bed8c9c2593
spalnique <lex.sydoruk@gmail.com>
2024-03-13 22:06:31 +0100

    upd

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,28 +2,24 @@ import Counter from '../Counter/Counter';
 
 const Feedback = ({ feedbackData, totalFeedbacks, positiveRate }) => {
   return (
-    <>
-      <ul>
-        {Object.keys(feedbackData).map((feedbackType) => {
-          return (
-            <li key={feedbackType}>
-              <Counter
-                feedbackType={feedbackType}
-                value={feedbackData[feedbackType]}
-              />
-            </li>
-          );
-        })}
-      </ul>
-      <ul>
-        <li key={'total'}>
-          <Counter feedbackType={'total'} value={totalFeedbacks} />
-        </li>
-        <li key={'positive'}>
-          <Counter feedbackType={'positive'} value={positiveRate + '%'} />
-        </li>
-      </ul>
-    </>
+    <ul>
+      {Object.keys(feedbackData).map((feedbackType) => {
+        return (
+          <li key={feedbackType}>
+            <Counter
+              feedbackType={feedbackType}
+              value={feedbackData[feedbackType]}
+            />
+          </li>
+        );
+      })}
+      <li key={'total'}>
+        <Counter feedbackType={'total'} value={totalFeedbacks} />
+      </li>
+      <li key={'positive'}>
+        <Counter feedbackType={'positive'} value={positiveRate + '%'} />
+      </li>
+    </ul>
   );
 };
 
